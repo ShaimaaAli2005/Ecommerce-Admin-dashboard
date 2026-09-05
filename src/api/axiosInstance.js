2bd71d2616671b859ff33c0a7eacb68ec56ea85b
shahd <malk@123>
2026-09-06 01:46:54 +0300

    modified baseURL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL || 'https://e-commerce-api-3wara.vercel.app' ,
-  baseURL: import.meta.env.VITE_API_BASE_URL  ,
+  baseURL: 'https://e-commerce-api-3wara.vercel.app' ,
+  // baseURL: import.meta.env.VITE_API_BASE_URL  ,
 
   headers: {
     'Content-Type': 'application/json',
