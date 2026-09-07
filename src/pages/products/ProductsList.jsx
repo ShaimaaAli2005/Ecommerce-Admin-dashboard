18b2244c7819efb072fbb73f397a2cd195b333a6
shahd <malk@123>
2026-09-07 15:26:14 +0300

    ditales in cards

--- src/pages/products/ProductsList.jsx
+++ src/pages/products/ProductsList.jsx
@@ -11,7 +11,7 @@ export default function ProductList() {
     const [products, setProducts] = useState([
         { id: 1, name: 'Vintage Brown Leather Watch', price: 150, category: 'Watches', rating: 4.5, image: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500' },
 { id: 2, name: 'Rose Gold Smartwatch', price: 230, category: 'Watches', rating: 2.5, image: 'https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?w=500' },
-{ id: 3, name: 'Black Steel Chronograph', price: 190, category: 'Watches', rating: 3.2, image: 'https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?w=500' },
+{ id: 3, name: 'Black Steel Chronograph', price: 190, category: 'Accessories', rating: 3.2, image: 'https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?w=500' },
 { id: 4, name: 'Ford Mustang GT', price: 120, category: 'Cars', rating: 3.2, image: 'https://images.unsplash.com/photo-1552519507-da3b142c6e3d?w=500' },
 { id: 5, name: 'Porsche Spider', price: 120, category: 'Cars', rating: 3, image: 'https://images.unsplash.com/photo-1583121274602-3e2820c69888?w=500' },
 { id: 6, name: 'Golden Diamond Necklace', price: 450, category: 'Accessories', rating: 5, image: 'https://images.unsplash.com/photo-1535632066927-ab7c9ab60908?w=500' },
@@ -19,7 +19,7 @@ export default function ProductList() {
 { id: 8, name: 'Designer Leather Wallet', price: 60, category: 'Accessories', rating: 4.2, image: 'https://images.unsplash.com/photo-1627123424574-724758594e93?w=500' },
 { id: 9, name: 'Lamborghini Aventador', price: 500, category: 'Cars', rating: 4.9, image: 'https://images.unsplash.com/photo-1544829099-b9a0c07fad1a?w=500' },
 { id: 10, name: 'BMW M4 Coupe', price: 280, category: 'Cars', rating: 4.6, image: 'https://images.unsplash.com/photo-1555215695-3004980ad54e?w=500' },
-{ id: 11, name: 'Crystal Pearl Bracelet', price: 75, category: 'Accessories', rating: 4.4, image: 'https://images.unsplash.com/photo-1611591475271-1d521d8b9288?w=500' },
+{ id: 11, name: 'BMW M4 Coupe', price: 75, category: 'Cars', rating: 4.4, image: 'https://images.unsplash.com/photo-1611591475271-1d521d8b9288?w=500' },
 { id: 12, name: 'Aston Martin Vantage', price: 420, category: 'Cars', rating: 4.9, image: 'https://images.unsplash.com/photo-1617814076367-b759c7d7e738?w=500' },
 { id: 13, name: 'Elegant Diamond Earrings', price: 195, category: 'Accessories', rating: 4.8, image: 'https://images.unsplash.com/photo-1630019852942-f89202989a59?w=500' },
 { id: 14, name: 'Modern Sunglasses', price: 90, category: 'Accessories', rating: 4.3, image: 'https://images.unsplash.com/photo-1511499767150-a48a237f0083?w=500' }
@@ -69,6 +69,8 @@ export default function ProductList() {
     const startIndex = (page - 1) * itemsPerPage
     const endIndex = startIndex + itemsPerPage
     const currentProducts = filteredProducts.slice(startIndex,endIndex) 
+
+    const numOfPage = Math.ceil(filteredProducts.length / itemsPerPage)
 
     return (
         <div className="p-6 max-w-7xl mx-auto min-h-screen" style={{ backgroundColor: '#F7F5F0' }}>
@@ -175,10 +177,12 @@ export default function ProductList() {
                             </div>
 
                             {/* Price & Rating Footer */}
-                            <div className="mt-4 flex items-center justify-between pt-3 border-t border-[#E5E7EB]">
-                                <span className="font-bold text-lg" style={{ color: '#E89A5B' }}>
-                                    ${product.price}
-                                </span>
+                            <div className="mt-4  flex flex-col gap-3">
+
+                                <div className="flex items-center justify-between">
+                                   <span className="font-bold text-lg" style={{ color: '#E89A5B' }}>
+                                       ${product.price}
+                                    </span>
                                 
                                 <div className="flex items-center gap-1">
                                     <span className="text-amber-500 text-sm">⭐</span>
@@ -186,6 +190,40 @@ export default function ProductList() {
                                         {product.rating || '4.5'}
                                     </span>
                                 </div>
+                            </div>
+                                 
+                                <div className="flex items-center flex-wrap gap-1.5 pb-5">
+                                    <span className="rounded-lg border border-slate-200 bg-slate-100 px-2.5 py-1 text-xs
+                                    text-slate-500 dark:border-slate-700 dark:bg-slate-800 dark:text-slate-400 ">
+                                       updated 
+                                    </span>
+                                    <span className="rounded-lg border border-slate-200 bg-slate-100 px-2.5 py-1 text-xs
+                                    text-slate-500 dark:border-slate-700 dark:bg-slate-800 dark:text-slate-400 ">
+                                       pro 
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div className="border-t border-[#E5E7EB] mt-auto flex flex-wrap gap-2">
+                               <button className="flex items-center gap-1.5 rounded-xl border px-4 py-2 mt-3
+                               text-xs font-semibold">
+                                   View
+                               </button>
+
+                                 <button className="flex items-center gap-1.5 rounded-xl border px-4 py-2 mt-3
+                               text-xs font-semibold">
+                                   Edit
+                               </button>
+
+                                 <button className="flex items-center gap-1.5 rounded-xl border px-4 py-2 mt-3
+                               text-xs font-semibold">
+                                   Qick Edit
+                               </button>
+
+                                 <button className="ml-auto flex items-center gap-1.5 rounded-xl border border-rose-200 
+                                 px-4 py-2 mt-3 bg-rose-50 text-xs font-semibold">
+                                   Delete
+                               </button>
                             </div>
                         </div>
                     ))}
@@ -217,16 +255,19 @@ export default function ProductList() {
                 <span className="font-medium" style={{ color: '#7B8190' }}>page: {page}</span>
                 <button
                     onClick={() => setPage((prev) => prev + 1)}
-                    className="px-4 py-2 text-white font-medium transition cursor-pointer"
+                    disabled = {page >= numOfPage}
+                    className="px-4 py-2 text-white font-medium transition cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
                     style={{
                         backgroundColor: '#17233C',
-                        borderRadius: '10px'
+                        borderRadius: '10px',
                     }}
+                    
                     onMouseEnter={(e) => e.target.style.backgroundColor = '#E89A5B'}
                     onMouseLeave={(e) => e.target.style.backgroundColor = '#17233C'}
                 >
                     next
                 </button>
+                
             </div>
         </div> 
     );
